6cbbcebe0f4b243dc5750216d278dbfa463ac68e
dxryookada <ryookada@tokyo-gas.co.jp>
2024-12-19 11:45:00 +0900

    survey_rink_maked

--- voc_app/views.py
+++ voc_app/views.py
@@ -3,3 +3,22 @@ from django.views.generic import TemplateView
 
 class PublishView(TemplateView):
   template_name = "survey/publish.html"
+
+class QrView(TemplateView):
+  template_name = "survey/qr.html"
+
+  def post(self, request, *args, **kwargs):
+     # POSTリクエストが送信されたときに処理を行いたい場合はここで行います。
+     # 必要な処理を追加してください。 
+     return self.get(request, *args, **kwargs)
+
+class SurveyView(TemplateView):
+  template_name = "survey/survey.html"
+
+class ComView(TemplateView):
+  template_name = "survey/complete.html"
+
+  def post(self, request, *args, **kwargs):
+    # POSTリクエストが送信されたときに処理を行いたい場合はここで行います。
+    # 必要な処理を追加してください。 
+    return self.get(request, *args, **kwargs)
