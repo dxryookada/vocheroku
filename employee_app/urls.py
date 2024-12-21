8c2efefa9dc1b99c1b270804a8ada77c39e71245
kazuya-0914 <kazu.work.0914@gmail.com>
2024-12-21 19:16:39 +0900

    12/21 コミット

--- employee_app/urls.py
+++ employee_app/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
   path('', views.LoginView.as_view(), name='login'),
+  # --- 一般ユーザー
+  path('general', views.GeneralView.as_view(), name='general'),
+  path('update', views.UpdateView.as_view(), name='update'),
+  # --- 管理者
+  path('admin', views.AdminView.as_view(), name='admin'),
 ]
--- employee_app/views.py
+++ employee_app/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+# --- ログインページ
 class LoginView(TemplateView):
   template_name = "login.html"
 
+# --- 一般従業員ページ
+class GeneralView(TemplateView):
+  template_name = "general/general.html"
+
+class UpdateView(TemplateView):
+  template_name = "general/update.html"
+
+# --- 管理者ページ
+class AdminView(TemplateView):
+  template_name = "admin/admin.html"
